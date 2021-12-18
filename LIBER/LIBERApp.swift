49b090d591317280d157c41f0d2aa36bab427a07
SreekuttanCL <sreekuttancl12@gmail.com>
2021-12-18 14:02:25 -0400

    Initial Commit

---
+++ LIBER/LIBERApp.swift
@@ -0,0 +1,17 @@
+//
+//  LIBERApp.swift
+//  LIBER
+//
+//  Created by Sreekuttan C L on 2021-12-18.
+//
+
+import SwiftUI
+
+@main
+struct LIBERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
